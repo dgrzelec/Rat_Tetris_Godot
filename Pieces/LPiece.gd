3b8a6f7a46793b70cfc50767a56e51b3eda7d932
Daniel Grzelec <danielgrzelec@gmail.com>
2023-02-20 17:55:22 +0100

    LPiece and ReverseLPiece rotation added

--- Pieces/LPiece.gd
+++ Pieces/LPiece.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var blocks = [$U, $L, $R, $B]
+onready var blocks = [$U, $L, $B, $R]
 
 func _ready():
 	pass
@@ -28,11 +28,30 @@ func move_right(increment):
 	for block in blocks:
 		block.move_right(increment)
 
-func rotate(_increment):
-	pass
-	
-func rotate_back(_increment):
-	pass
+var rotation_id = 0
+var rotation_vecs = [
+				[Vector2(0,-2), Vector2(-1,-1),Vector2.ZERO,Vector2(1,1)],
+				[Vector2(2,0), Vector2(1,-1),Vector2.ZERO,Vector2(-1,1)],
+				[Vector2(0,2), Vector2(1,1), Vector2.ZERO, Vector2(-1,-1)],
+				[Vector2(-2,0), Vector2(-1,1), Vector2.ZERO, Vector2(1,-1)]]
+
+
+func rotate(increment):
+	rotation_id += 1
+	if rotation_id > 3:
+		rotation_id = 0
+	#apply rotation
+	for i in 4:
+		var mod_vec = rotation_vecs[rotation_id][i]
+		blocks[i].rotation_degrees = 90 * rotation_id	
+		blocks[i].position += mod_vec * increment
+		blocks[i].x += mod_vec.x
+		blocks[i].y += mod_vec.y
+		
+
+func rotate_back(increment):
+	for i in 3:
+		rotate(increment)
 	
 func get_blocks():
 	return blocks.duplicate(true)
